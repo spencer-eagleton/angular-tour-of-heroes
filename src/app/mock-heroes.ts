256cecf5dcfbbe1fcce7afb5d3d1f129a9776dfd
seagleton <seagleton@basspro.com>
2023-03-01 16:05:39 -0600

    got heroes displayed listed and styled

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Silly' },
+  { id: 13, name: 'Wrekdzor' },
+  { id: 14, name: 'Carrot-person' },
+  { id: 15, name: 'Yellowbelly' },
+  { id: 16, name: 'JellyBoy' },
+  { id: 17, name: 'Human Snail' },
+  { id: 18, name: 'Tim' },
+  { id: 19, name: 'The Corrector' },
+  { id: 20, name: 'Jumbler' }
+];
