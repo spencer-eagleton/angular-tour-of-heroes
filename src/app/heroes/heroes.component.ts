0c2802611eec5b92ef4b7ecba1eda79686207728
seagleton <seagleton@basspro.com>
2023-03-02 11:36:42 -0600

    getHeroes observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,7 +20,8 @@ export class HeroesComponent {
   selectedHero?: Hero;
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
