3dd06e357f6a3cb06cd2fe9ba9dfac3dc9060013
seagleton <seagleton@basspro.com>
2023-03-02 16:39:43 -0600

    inject message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from "../hero";
 import {HeroService} from "../hero.service";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +10,7 @@ import {HeroService} from "../hero.service";
 })
 
 export class HeroesComponent {
-  constructor(private heroService: HeroService) {
+  constructor(private heroService: HeroService, private messageService: MessageService) {
   }
 
   ngOnInit(): void {
@@ -26,6 +27,7 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 }
 
